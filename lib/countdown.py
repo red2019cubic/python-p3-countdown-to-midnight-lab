723148ed3a35ecf9dc3aa79adb12e377111f82b0
Red <red.sarouf2014@gmail.com>
2023-07-15 13:01:06 -0400

    add python countdown to midnight lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,27 @@
 # your code goes here!
+import time
+
+def countdown(num):
+    while num >= 0:
+        
+        if num > 0:
+            print(f"{num} SECOND(S)!")
+            
+        elif num == 0:
+            print("HAPPY NEW YEAR!")
+            break
+        num -= 1
+def countdown_with_sleep(num):
+    while num >= 0:
+        
+        if num == 0:
+            print("HAPPY NEW YEAR!")
+            break
+        elif num > 0 :
+            print(f"{num} SECOND(S)!")
+            time.sleep(1)
+        num -= 1
+        
+    
+# countdown(5)
+countdown_with_sleep(5)
